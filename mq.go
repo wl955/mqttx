555f764735c083e26d9f63303a022de340db6e4c
jq1 <jq.go@qq.com>
2023-01-17 17:00:15 +0800

    modify mq

--- mq.go
+++ mq.go
@@ -9,27 +9,7 @@ var _opts = mqtt.NewClientOptions()
 
 var client mqtt.Client
 
-//func init() {
-//	opts := mqtt.NewClientOptions()
-//
-//	opts.AddBroker("tcp://broker.emqx.io:1883")
-//
-//	opts.SetClientID("go_mqtt_client1")
-//
-//	opts.SetDefaultPublishHandler(pubHandler)
-//	opts.SetOnConnectHandler(connectHandler)
-//	opts.SetConnectionLostHandler(connectLostHandler)
-//
-//	client = mqtt.NewClient(opts)
-//
-//	if token := client.Connect(); token.Wait() && token.Error() != nil {
-//		panic(token.Error())
-//	}
-//
-//	return
-//}
-
-func Init(opts ...Option) (mqtt.Client, error) {
+func Init(opts ...Option) (e error) {
 	custom := Options{}
 
 	for _, opt := range opts {
@@ -45,27 +25,22 @@ func Init(opts ...Option) (mqtt.Client, error) {
 	)
 
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		return client, token.Error()
+		return token.Error()
 	}
 
-	return client, nil
-}
-
-//func Disconnect() {
-//	if client != nil {
-//		client.Disconnect(250)
-//	}
-//}
-
-func Serve() (e error) {
-	var token mqtt.Token
 	for _, route := range routes {
-		if token = client.Subscribe(route.topic, route.qos, route.callback); token.Wait() && token.Error() != nil {
-			e = token.Error()
-			break
+		if token := client.Subscribe(route.topic, route.qos, route.callback); token.Wait() && token.Error() != nil {
+			return token.Error()
 		}
 	}
+
 	return
+}
+
+func Disconnect() {
+	if client != nil {
+		client.Disconnect(250)
+	}
 }
 
 func Sub(topic string, qos byte, callback mqtt.MessageHandler) {
@@ -75,10 +50,6 @@ func Sub(topic string, qos byte, callback mqtt.MessageHandler) {
 		callback: callback,
 	})
 }
-
-//func Sub(topic string, qos byte, callback mqtt.MessageHandler) mqtt.Token {
-//	return client.Subscribe(topic, qos, callback)
-//}
 
 func Pub(topic string, qos byte, retained bool, payload interface{}) mqtt.Token {
 	return client.Publish(topic, qos, retained, payload)
